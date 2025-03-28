1a00247fb4d944690513e52af8b133a826396dd0
OmarValdes <144852754+OmarValdes@users.noreply.github.com>
2025-03-27 20:42:31 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // Registrar el Service Worker
 if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('/sw.js')
+    navigator.serviceWorker.register('/Asking_APP/sw.js')
         .then(reg => console.log('Service Worker registrado correctamente', reg))
         .catch(err => console.error('Error al registrar el Service Worker', err));
 }
