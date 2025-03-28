a6f7a8af0a4436d4eb927a1ac5887cf1dc0beafc
OmarValdes <144852754+OmarValdes@users.noreply.github.com>
2025-03-28 12:44:43 -0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -56,4 +56,26 @@ self.addEventListener('fetch', (event) => {
             caches.match(event.request).then(response => response || fetch(event.request))
         );
     }
+
+    self.addEventListener('fetch', (event) => {
+    if (event.request.url.endsWith('app-debug.apk')) {
+        event.respondWith(
+            caches.match(event.request).then((response) => {
+                // Si está en caché, devolverlo
+                if (response) {
+                    return response;
+                }
+                // Si no está en caché, intentar descargarlo
+                return fetch(event.request).then((networkResponse) => {
+                    // Guardar en la caché para futuras ocasiones
+                    return caches.open(CACHE_NAME).then((cache) => {
+                        cache.put(event.request, networkResponse.clone());
+                        return networkResponse;
+                    });
+                });
+            })
+        );
+    }
+});
+
 });
